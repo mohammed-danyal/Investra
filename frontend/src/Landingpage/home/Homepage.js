d4b8b4d7dcd153d5fb68019b853fbcdc81387ccc
Yashaswini Mullalli <yashaswinimullalli4@gmail.com>
2026-05-19 23:03:09 +0530

    Implement full-stack features: Auth, Live API, Dashboard Charts, and CRUD

--- frontend/src/Landingpage/home/Homepage.js
+++ frontend/src/Landingpage/home/Homepage.js
@@ -5,21 +5,21 @@ import Stats from './Stats';
 import Pricing from './Pricing';
 import Education from './Education';
 import Openacc from '../Openacc';
-import Footer from '../Footer';
-import Navbar from '../Navbar';
 
 
 function Homepage() {
-    return ( <>
-    <Hero/>
-    <Awards/>
-    <Stats/>
-    <Pricing/>
-    <Education/>
-    <Openacc/>
-    <Footer/>
-    <Navbar/>
-    </> );
-} 
+  return (
+    <>
+     
+      <Hero />
+      <Awards />
+      <Stats />
+      <Pricing />
+      <Education />
+      <Openacc />
+     
+    </>
+  );
+}
 
 export default Homepage;
